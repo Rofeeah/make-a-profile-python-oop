97e85fe4be245b3666c6ee3725010b46e02e88d4
Rofeeah <ayenirofeeah@gmail.com>
2021-07-12 14:39:31 +0000

    Add completed profile lab

--- profile_class.py
+++ profile_class.py
@@ -1,2 +1,14 @@
 # View the readme on Github to get more information on how to start this definition. 
 class Profile:
+    def __init__(self, name, birthday_month, address, age):
+        self.name = name
+        self.birthday_month = birthday_month
+        self.address = address
+        self.age = age
+        print("class profile" + self.name + "created!")
+
+
+# grace = Profile("Grace", "april", "New york", 18)
+# print(grace.age)
+
+
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 # View the readme on GitHub for more information about how to start this challenge. 
 import profile_class
+
+
+first_profile = profile_class.Profile("Grace", "april", "New york", 18)
+print(first_profile)
+
+second_profile = profile_class.Profile("Rofeeah", "March", "New york", 18)
+print(second_profile)
